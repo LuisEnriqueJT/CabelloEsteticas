446f6719882eab7837a67ae5c076894fb2091182
LuisEnriqueJT <luisenrique.jt@outlook.com>
2020-04-06 00:08:47 -0500

    Primera carga del proyecto Estetica Cabellos

---
+++ citas.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/adicional.css">
+	<link href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	
+	<title>Inicio</title>
+</head>
+<body>
+	
+	<div class="container header">
+		<div class="row pt-4">
+			<h1 class="tipografia1">Cabello Esteticas</h1>
+		</div>
+			<h5 class="">Somos profesionales de la belleza</h5>
+	</div>
+
+	<nav class="navbar navbar-light bg-dark justify-content-around">
+		<a class="nav-link" href="">Inicio</a>
+		<a class="nav-link" href="">Productos</a>
+		<a class="nav-link" href="citas.php">Agenda una cita</a>
+		<a class="nav-link" href="">¿Quienes somos?</a>
+
+	</nav>
+
+	<div class="container-fluid footer">
+		<div class="row justify-content-center">
+			
+			<div class="col align-items-center">Derechos Reservados &copy 2020 <br>Luis Enrique Jimenez</div>
+			
+		</div>
+	</div>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
